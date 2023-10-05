33a4260be39044c7caccc81d97f4ec4d8db26beb
unknown <you@example.com>
2023-10-05 18:00:06 -0500

    first juli

--- src/controllers/venta.controllers.js
+++ src/controllers/venta.controllers.js
@@ -11,8 +11,8 @@ export const VentaInsertar= async(req,res)=>{
 export const MostrarVentaPorIdmesa = async (req, res) => 
 {
     const {id_mesa}=req.params;
-    const [result]=await pool.query('select *  from venta WHERE id_mesa=?',[id_mesa]);
-    res.send(result);
+    const [result]=await pool.query('call VENTA__MostrarID(?)?',[id_mesa]);
+    res.send(result[0]);
 }
 
 export const ventamostrarcuenta = async (req, res) => 
